b3f5bb23030a2e00a15a28171e9ea28dbefe39c6
Muhammad Taha <wwwtahataha343@gmail.com>
2025-02-18 21:42:15 +0500

    Add files via upload

---
+++ positive and negitive number tester.cpp
@@ -0,0 +1,14 @@
+//TAKE A NUMBER FROM USER AND TELL IF IT IS POSITIVE OR NEGITIVE
+#include<iostream>
+using namespace std;
+int main(){
+
+
+int a;
+cout<<"enter your number ";
+cin>>a;
+if(a<0)
+cout<<("the number is negitive");
+else
+cout<<("the number is positive");
+}
