4788e41b2bf63bdc211c795eedfd2d63497a364a
Muhammad Taha <wwwtahataha343@gmail.com>
2025-02-18 21:43:30 +0500

    Add files via upload

---
+++ citizen discount.cpp
@@ -0,0 +1,14 @@
+// ELIGIBILITY TESTER TO CHEAK IF THE USER IS ELIGIBLE TO CITIZEN DISCOUNT
+#include <iostream>
+using namespace std;
+
+int main() {
+   int age;
+   cout<<"enter your age=:";
+   cin>>age;
+   if(age>60){
+   	cout<<"your eligible for the discount (*-*)";
+   	
+   }else cout<<"your not eligible for the discount sorry mate ";
+}
+
