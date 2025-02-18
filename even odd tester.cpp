63b8384cea8cb1357dc28c70a8f118915649046e
Muhammad Taha <wwwtahataha343@gmail.com>
2025-02-18 21:45:59 +0500

    Add files via upload

---
+++ even odd tester.cpp
@@ -0,0 +1,14 @@
+//WRITE A PROGRAM TO CHEAK IF THE NUMBER ENTER BY THE USER IS EVEN OR ODD
+#include<iostream>
+using namespace std;
+int main(){
+	int a;
+	cout<<"ENTER YOUR VALUE =: ";
+	cin>>a;
+	if (a%2==0)
+	{
+		cout<<"the number  "<<a<<" " <<"is even sir!";
+	}
+else 
+cout<<"the number "<<a<<" " << "is odd sir! ";
+}
